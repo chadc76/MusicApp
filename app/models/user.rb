6ba2aba068cc52068e3e05846022167360852366
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-20 09:18:42 -0400

    Added Usaers validations and methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,42 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
-  validates :email, :session_token, presence: true
-  validates :email, :session_token, :password_digest, uniqueness: true
+  attr_reader :password
+
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize: ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def reset_session_token!
+    self.session_token = nil
+    self.save
+    self.class.generate_session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
 end
