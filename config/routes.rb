a22e12c582e164091caf2240e0bbf2b7c0e4128e
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-20 11:05:57 -0400

    Added root

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i(show new create)
   resource :session, only: %i(new create destroy)
+
+  root to: 'sessions#new'
 end
