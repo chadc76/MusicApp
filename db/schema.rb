181df54d9bd581b3192c19fd1f3d9328b217ab67
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-22 08:10:07 -0400

    Added albums table along with band association

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,16 @@ ActiveRecord::Schema.define(version: 2021_05_22_111803) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "albums", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "year", null: false
+    t.integer "band_id", null: false
+    t.boolean "live", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["band_id"], name: "index_albums_on_band_id"
+  end
+
   create_table "bands", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
