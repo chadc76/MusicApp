0e35b0fc0650ccd1035ad56737e4e1efe506c5a0
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-22 08:14:05 -0400

    Added routes for albums

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i(show new create)
   resource :session, only: %i(new create destroy)
-  resources :bands
-
+  resources :bands do
+    resources :albums, only: [:new]
+  end
+  resources :albums, except: %i(new index)
+  
   root to: 'sessions#new'
 end
