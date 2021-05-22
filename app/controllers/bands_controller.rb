4d2ed564c154950394d559e26fd502a719af87ab
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-22 07:56:38 -0400

    Added Edit and Update actions and edit view

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -21,8 +21,24 @@ class BandsController < ApplicationController
     if @band.save 
       redirect_to band_url(@band)
     else
-      flash[:errors] = @band.errors.full_messages
+      flash.now[:errors] = @band.errors.full_messages
       render :new
+    end
+  end
+
+  def edit
+    @band = Band.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @band = Band.find(params[:id])
+    
+    if @band.update(band_params)
+      redirect_to band_url(@band)
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :edit
     end
   end
 
