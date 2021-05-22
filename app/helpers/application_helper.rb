64b2676bda76638839c48b186b0f149f523bd356
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-22 09:44:04 -0400

    Added band selector helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def auth_token
     "<input type=\"hidden\" name=\"authenticity_token\" value=\"#{form_authenticity_token}\">".html_safe
   end
+
+  def band_selector(album, band)
+    if album.persisted?
+      selected = album.band_id == band.id ? "selected" : ""
+    else
+      selected = current_band.id == band.id ? "selected" : ""
+    end
+
+    selected
+  end
 end
