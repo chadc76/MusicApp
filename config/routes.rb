fe2513d73f97c38242ab7451f2306c74ea471c7e
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-26 10:18:27 -0400

    Added Tracks controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i(show new create)
+
   resource :session, only: %i(new create destroy)
+
   resources :bands do
     resources :albums, only: [:new]
   end
-  resources :albums, except: %i(new index)
+
+  resources :albums, except: %i(new index) do
+    resources :tracks, only: [:new]
+  end
+
+  resources :tracks, except: %i(new index)
   
   root to: 'sessions#new'
 end
