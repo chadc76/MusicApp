32da1421f43cfcb375f34b7efaea4233a15747cc
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-26 11:10:05 -0400

    Added Notes Table

---
+++ app/models/note.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id         :bigint           not null, primary key
+#  content    :text             not null
+#  user_id    :integer          not null
+#  track_id   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Note < ApplicationRecord
+end
