1053a84f150dd30eb096e30705aaf7012d59c212
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-26 10:21:53 -0400

    Added all of the actions

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,3 +1,49 @@
 class TracksController < ApplciationController
+  def show
+    @track = Track.find_by(id: params[:id])
+    render :show
+  end
 
+  def new
+    @track = Track.new
+    render :new
+  end
+
+  def create
+    @track = Track.new(track_params)
+    if @track.save
+      redirect_to track_url(@track)
+    else
+      flash.now[:errors] = @track.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @track = Track.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @track = Track.find_by(id: params[:id])
+    if @track.update(track_params)
+      redirect_to track_url(@track)
+    else
+      flash.now[:errors] = @track.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @track = Track.find_by(id: params[:id])
+    @track.destroy
+    flash[:notice] = ["#{@track.title} has been deleted!"]
+    redirect_to album_url(@track.album_id)
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:title, :ord, :album_id, :bonus_track, :lyrics)
+  end
 end
