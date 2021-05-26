3566878c7ec4f0405ebbf59cdea2c3853d732ceb
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-26 11:55:26 -0400

    Added Admin Column to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_152103) do
+ActiveRecord::Schema.define(version: 2021_05_26_155409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_05_26_152103) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "activated", default: false
     t.string "activation_token", null: false
+    t.boolean "admin", default: false
     t.index ["activation_token"], name: "index_users_on_activation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
